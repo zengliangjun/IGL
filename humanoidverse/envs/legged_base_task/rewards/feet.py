f4df33adbc3352cde8a0e678efd5e0f95e03bccc
liangjun <liangjunzeng@163.com>
2025-04-19 07:56:32 +0800

    #Fix clear feed rewards

--- humanoidverse/envs/legged_base_task/rewards/feet.py
+++ humanoidverse/envs/legged_base_task/rewards/feet.py
@@ -1,6 +1,8 @@
 import torch
 from humanoidverse.envs.base_task.term import base
 from humanoidverse.utils.torch_utils import quat_rotate_inverse
+from humanoidverse.utils.torch_utils import quat_rotate_inverse, quat_apply
+from isaac_utils.rotations import wrap_to_pi
 
 class FeetRewards(base.BaseManager):
     def __init__(self, _task):
@@ -40,6 +42,18 @@ class FeetRewards(base.BaseManager):
         second_foot_contact = feet_manager.contact_filt[:, 1]
         reward = ~(first_foot_contact | second_foot_contact)
         return reward
+
+    def _reward_feet_air_time(self):
+        # Reward long steps
+        # Need to filter the contacts because the contact reporting of PhysX is unreliable on meshes
+        feet_manager = self.task.feet_manager
+        command_manager = self.task.command_manager
+
+        first_contact = (feet_manager.feet_air_time > self.task.dt) * feet_manager.contact_filt
+
+        rew_airTime = torch.sum((feet_manager.feet_air_time - 0.5) * first_contact, dim=1) # reward only on first contact with the ground
+        rew_airTime *= torch.norm(command_manager.commands[:, :2], dim=1) > 0.1 #no reward for zero command
+        return rew_airTime
 
     def _reward_feet_max_height_for_this_air(self):
         # Reward long steps
@@ -83,6 +97,29 @@ class FeetRewards(base.BaseManager):
         right_quat = self.task.simulator._rigid_body_rot[:, robotdata_manager.feet_indices[1]]
         right_gravity = quat_rotate_inverse(right_quat, robotstatus_manager.gravity_vec)
         return torch.sum(torch.square(left_gravity[:, :2]), dim=1)**0.5 + torch.sum(torch.square(right_gravity[:, :2]), dim=1)**0.5
+
+    #def feet heading alignment(self):
+    def _reward_feet_heading_alignment(self):
+        robotstatus_manager = self.task.robotstatus_manager
+        robotdata_manager = self.task.robotdata_manager
+        command_manager = self.task.command_manager
+
+        left_quat = self.task.simulator._rigid_body_rot[:, robotdata_manager.feet_indices[0]]
+        right_quat = self.task.simulator._rigid_body_rot[:, robotdata_manager.feet_indices[1]]
+
+        forward_left_feet = quat_apply(left_quat, command_manager.forward_vec)
+        heading_left_feet = torch.atan2(forward_left_feet[:, 1], forward_left_feet[:, 0])
+        forward_right_feet = quat_apply(right_quat, command_manager.forward_vec)
+        heading_right_feet = torch.atan2(forward_right_feet[:, 1], forward_right_feet[:, 0])
+
+
+        root_forward = quat_apply(robotstatus_manager.base_quat, command_manager.forward_vec)
+        heading_root = torch.atan2(root_forward[:, 1], root_forward[:, 0])
+
+        heading_diff_left = torch.abs(wrap_to_pi(heading_left_feet - heading_root))
+        heading_diff_right = torch.abs(wrap_to_pi(heading_right_feet - heading_root))
+
+        return heading_diff_left + heading_diff_right
 
     ## feet height
     def _reward_penalty_feet_height(self):
